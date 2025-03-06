8a832f7663c6b42c05c6631a6f5ef25df4636dac
Benjamin Allen <110572177+benwillallen@users.noreply.github.com>
2025-03-06 14:47:02 -0600

    Popup Changed

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 // popup.js
-document.getElementById("highlight").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      files: ["content.js"]
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("highlight").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["content.js"]
+      });
     });
   });
-})
+});
